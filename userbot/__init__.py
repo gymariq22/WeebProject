f84c4a9e6ebcfac36f523d7f55cce7aa2eed630f
gymariq22 <40758519+gymariq22@users.noreply.github.com>
2022-03-20 08:04:00 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -80,7 +80,7 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
-    "UPSTREAM_REPO_URL", "https://github.com/BianSepang/WeebProject.git"
+    "UPSTREAM_REPO_URL", "https://github.com//WeebProject.git"
 )
 # UPSTREAM_REPO_URL branch, the default is master
 UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "master")
@@ -97,7 +97,7 @@ OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
 # Default .alive name anf logo
 ALIVE_NAME = os.environ.get("ALIVE_NAME") or None
 ALIVE_LOGO = (
-    os.environ.get("ALIVE_LOGO") or "https://telegra.ph/file/9e888cb1e485adf3a179e.mp4"
+    os.environ.get("ALIVE_LOGO") or "https://telegra.ph/file/96b4ee66d39423680eda5.jpg"
 )
 
 # remove.bg API key
